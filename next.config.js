037a0a0e79d4f5c5249d467a0f018954cc65496b
Paulo Dantas <paulo.roberto@hdn.digital>
2022-02-28 14:20:03 -0300

    Deploy fix - update baseUrl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://www.devux.com.br",
     MONGODB_URL:
       "mongodb+srv://devux:devux@cluster0.osmxz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "Dev-UX-Token-Secret",
