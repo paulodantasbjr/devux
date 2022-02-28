f88c8acd64ce1e035a206184b37922cdea067c5a
Paulo Dantas <paulo.roberto@hdn.digital>
2022-02-28 14:51:26 -0300

    Deploy fix - Problem with name of DB

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ module.exports = {
   env: {
     BASE_URL: "https://www.devux.com.br",
     MONGODB_URL:
-      "mongodb+srv://devux:devux@cluster0.osmxz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "mongodb+srv://devux:devux@cluster0.osmxz.mongodb.net/devux?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "Dev-UX-Token-Secret",
     REFRESH_TOKEN_SECRET: "Dev-UX-RefreshToken-Secret",
   },
