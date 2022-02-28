8c6861ffdcdbab5c3b4bf2ee68413a0eddd6c08b
Paulo Dantas <paulo.roberto@hdn.digital>
2022-02-28 14:46:29 -0300

    Deploy fix - Problem with cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   env: {
     BASE_URL: "https://www.devux.com.br",
     MONGODB_URL:
